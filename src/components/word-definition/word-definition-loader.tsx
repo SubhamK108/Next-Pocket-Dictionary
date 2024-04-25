4890d3716b5a3b13e87ebb03284c3f001df78dcb
Subham Karmakar <44800073+SubhamK108@users.noreply.github.com>
2024-04-25 21:54:38 +0530

    Tweaked some paddings & heights/widths

--- src/components/word-definition/word-definition-loader.tsx
+++ src/components/word-definition/word-definition-loader.tsx
@@ -19,31 +19,30 @@ export default function WordDefinitionLoader(): ReactElement {
           </div>
         </div>
       </div>
-      <div className="mb-16 max-sm:mb-10 w-[45rem] mx-auto max-sm:w-auto max-sm:mx-5 flex flex-col justify-start items-start text-left gap-5 max-sm:gap-4">
-        <div className="bg-zinc-300 dark:bg-zinc-800 p-4 max-sm:p-3 rounded-3xl shadow-xl">
-          <div className="w-[12rem] h-[3.3rem] max-sm:w-[8rem] max-sm:h-[2rem] bg-zinc-400 dark:bg-zinc-700 rounded-xl max-sm:rounded-lg shadow-lg dark:shadow-2xl text-[1.8rem] max-sm:text-[1.2rem] font-bold"></div>
+      <div className="mb-14 max-sm:mb-10 w-2/5 mx-auto max-sm:w-auto max-sm:mx-6 flex flex-col justify-start items-start text-left gap-5 max-sm:gap-4">
+        <div className="w-full max-sm:w-full bg-zinc-300 dark:bg-zinc-800 p-5 max-sm:p-3 rounded-3xl shadow-xl">
+          <div className="w-[12rem] h-[3.1rem] max-sm:w-[8rem] max-sm:h-[2rem] bg-zinc-400 dark:bg-zinc-700 rounded-xl max-sm:rounded-lg shadow-lg dark:shadow-2xl text-[1.8rem] max-sm:text-[1.2rem] font-bold"></div>
           <div>
             <div className="mt-5 max-sm:mt-3">
-              <div className="w-[45rem] h-[2.6rem] max-sm:w-[20rem] max-sm:h-[1.7rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.65rem] max-sm:text-[1.15rem]"></div>
-              <div className="mt-2 max-sm:mt-1 w-[33rem] h-[2.2rem] max-sm:w-[15rem] max-sm:h-[1.4rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl ml-9 max-sm:ml-6 text-[1.5rem] max-sm:text-[1.1rem] italic"></div>
+              <div className="w-full h-[2.4rem] max-sm:w-[20rem] max-sm:h-[1.7rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.65rem] max-sm:text-[1.15rem]"></div>
+              <div className="mt-2 max-sm:mt-1 w-[33rem] h-[2.1rem] max-sm:w-[15rem] max-sm:h-[1.4rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl ml-9 max-sm:ml-6 text-[1.5rem] max-sm:text-[1.1rem] italic"></div>
             </div>
             <div className="mt-5 max-sm:mt-3">
-              <div className="w-[45rem] h-[2.6rem] max-sm:w-[20rem] max-sm:h-[1.7rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.65rem] max-sm:text-[1.15rem]"></div>
+              <div className="w-11/12 h-[2.4rem] max-sm:w-[20rem] max-sm:h-[1.7rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.65rem] max-sm:text-[1.15rem]"></div>
             </div>
             <div className="ml-1 mt-5 max-sm:mt-3">
               <div className="w-[20rem] h-[1.8rem] max-sm:w-[7rem] max-sm:h-[1.15rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.4rem] max-sm:text-[1.05rem]"></div>
-              <div className="w-[25rem] h-[1.8rem] max-sm:w-[10rem] max-sm:h-[1.15rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.4rem] max-sm:text-[1.05rem] mt-2 max-sm:mt-1"></div>
             </div>
           </div>
         </div>
-        <div className="bg-zinc-300 dark:bg-zinc-800 p-4 max-sm:p-3 rounded-3xl shadow-xl">
-          <div className="w-[12rem] h-[3.3rem] max-sm:w-[8rem] max-sm:h-[2rem] bg-zinc-400 dark:bg-zinc-700 rounded-xl max-sm:rounded-lg shadow-lg dark:shadow-2xl text-[1.8rem] max-sm:text-[1.2rem] font-bold"></div>
+        <div className="w-full max-sm:w-full bg-zinc-300 dark:bg-zinc-800 p-4 max-sm:p-3 rounded-3xl shadow-xl">
+          <div className="w-[12rem] h-[3.1rem] max-sm:w-[8rem] max-sm:h-[2rem] bg-zinc-400 dark:bg-zinc-700 rounded-xl max-sm:rounded-lg shadow-lg dark:shadow-2xl text-[1.8rem] max-sm:text-[1.2rem] font-bold"></div>
           <div>
             <div className="mt-5 max-sm:mt-3">
-              <div className="w-[45rem] h-[2.6rem] max-sm:w-[20rem] max-sm:h-[1.7rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.65rem] max-sm:text-[1.15rem]"></div>
+              <div className="w-full h-[2.4rem] max-sm:w-[20rem] max-sm:h-[1.7rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.65rem] max-sm:text-[1.15rem]"></div>
             </div>
             <div className="mt-5 max-sm:mt-3">
-              <div className="w-[45rem] h-[2.6rem] max-sm:w-[20rem] max-sm:h-[1.7rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.65rem] max-sm:text-[1.15rem]"></div>
+              <div className="w-10/12 h-[2.4rem] max-sm:w-[20rem] max-sm:h-[1.7rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.65rem] max-sm:text-[1.15rem]"></div>
             </div>
             <div className="ml-1 mt-5 max-sm:mt-3">
               <div className="w-[20rem] h-[1.8rem] max-sm:w-[7rem] max-sm:h-[1.15rem] bg-zinc-400 dark:bg-zinc-700 rounded-lg max-sm:rounded-md shadow-lg dark:shadow-2xl text-[1.4rem] max-sm:text-[1.05rem]"></div>
